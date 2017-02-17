80b9acf15216bc38b6ed8d255ceec105fd22f9da
VVVVVincent <1194638485@qq.com>
2017-02-17 11:22:20 +0800

    项目创建
    项目创建

---
+++ iOSFrameWork/iOSFrameWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSFrameWork
+//
+//  Created by 李慧娟 on 17/2/17.
+//  Copyright © 2017年 中原集团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSFrameWork/iOSFrameWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSFrameWork
+//
+//  Created by 李慧娟 on 17/2/17.
+//  Copyright © 2017年 中原集团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
