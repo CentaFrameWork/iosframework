cbb22864feb097ecf2744d673dc00917ebf66893
VVVVVincent <1194638485@qq.com>
2017-03-20 10:44:05 +0800

    集成CocoaPods
    集成CocoaPods,添加SDWebImage第三方框架

---
+++ iOSFrameWork/Pods/Target Support Files/Pods-iOSFrameWork/Pods-iOSFrameWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSFrameWork : NSObject
+@end
+@implementation PodsDummy_Pods_iOSFrameWork
+@end
--- iOSFrameWork/iOSFrameWork/ViewController.m
+++ iOSFrameWork/iOSFrameWork/ViewController.m
@@ -17,7 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    //测试测试测试
 
 }
 
