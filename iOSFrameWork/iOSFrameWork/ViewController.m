b03b4f431aca1e7a2caee2c5f84b470fd0ebba8b
VVVVVincent <1194638485@qq.com>
2017-03-20 11:29:01 +0800

    多语言适配
    项目名称多语言适配、文本字符串多语言适配

--- iOSFrameWork/iOSFrameWork/ViewController.m
+++ iOSFrameWork/iOSFrameWork/ViewController.m
@@ -17,6 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.view.backgroundColor = [UIColor whiteColor];
+    UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 150)/2, 100, 150,50)];
+    textLabel.backgroundColor = [UIColor orangeColor];
+    textLabel.textColor = [UIColor whiteColor];
+    textLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:textLabel];
+    textLabel.text = NSLocalizedString(@"皮皮虾，我们走", @"");
+
 
 }
 
